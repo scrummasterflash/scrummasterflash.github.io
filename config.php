2d92deccd9b1723c005d769a53c8923629641f75
scrummasterflash <35874417+scrummasterflash@users.noreply.github.com>
2018-10-27 19:56:47 -0400

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$consumerKey = 'xEGRrBMHHGMkgXSCwpqQyvD4X';
-$consumerSecret = 'ZpxWH8RA5igcnQQJYJxhYOShZ8c08Gr5WZYBdsdVDZ8fUvO9tq';
-$accessToken = '1055959410563076096-HYwBQIISR6WTC8CIo6VK4yJTpFPyfl';
-$accessTokenSecret = 'C9RPxHGKzDQHfeJBTKkfF6R2Dws0vqccoHE41M8TqhTrp';
-?>
