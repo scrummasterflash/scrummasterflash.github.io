b44f4042bced343b3463d34c4ca5c729b510ae7f
scrummasterflash <35874417+scrummasterflash@users.noreply.github.com>
2018-10-27 20:00:34 -0400

    Add files via upload

---
+++ MyDynamicWebsite/src/SocialMediaIntegration.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST['submit'])){
+    $status = $_REQUEST['tweet'];
+    echo '<h2>The status is ' .$status .'</h2>';
+
+require "../WebContent/lib/twitteroauth/autoload.php";
+require "../config.php"
+    
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+$content = $connection->get("account/verify_credentials");
+
+
+$postStatus = $connection->post("statuses/update", ["status" => "hello world from SocialMediaIntegration.php"]);
+
+    echo '<h2>The tweet was sent. </h2>'
+    
+?>
