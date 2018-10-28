1be6a870c72ffb387d8abcb7cb65b4928069ecd7
scrummasterflash <35874417+scrummasterflash@users.noreply.github.com>
2018-10-27 20:47:06 -0400

    Add files via upload

--- MyDynamicWebsite/src/SocialMediaIntegration.php
+++ MyDynamicWebsite/src/SocialMediaIntegration.php
@@ -15,5 +15,8 @@ $content = $connection->get("account/verify_credentials");
 $postStatus = $connection->post("statuses/update", ["status" => "hello world from SocialMediaIntegration.php"]);
 
     echo '<h2>The tweet was sent. </h2>'
-    
+}
+else{
+    echo '<script>console.log("error")</script>';
+}
 ?>
